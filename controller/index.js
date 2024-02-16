caf145de0b71bdf5937c5ae95b6c7d9a6fd32d86
RichardWalag <126444200+RichardWalag@users.noreply.github.com>
2024-02-16 13:13:55 +0800

    simplify mock data; cleanup test command

--- controller/index.js
+++ controller/index.js
@@ -1,12 +1,12 @@
 const data = require('../mockData.json');
 
 const currencies = ( req, res ) => {
-  res.json( data )
+  res.json( data.map( d => d.code ) )
 }
 
 const currency = ( req, res ) => {
   const { code } = req.params;
-  const currency = data.currencies.find(currency => currency.code === code);
+  const currency = data.find(currency => currency.code === code);
   if ( currency !== undefined ) {
     return res.json(currency, 200);
   }
